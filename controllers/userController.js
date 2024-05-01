7ae1eef7e638d8cfe4555a469e9b09309f74a4de
lucas teixeira <91092558+DRAVINCK@users.noreply.github.com>
2024-04-30 21:10:52 -0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,38 +1,48 @@
 // ./controllers/userController.js
 
-class userController{
-    constructor(userService){
+class userController {
+    constructor(userService) {
         this.userService = userService;
     }
-    async create (req, res){ //request responde
-        const {nome, email, senha} = req.body;//sera alterado pq tem um erro de codigo
-        try{
+    async create(req, res) { //request responde
+        const { nome, email, senha } = req.body;//sera alterado pq tem um erro de codigo
+        try {
             const novoUser = await this.userService.cadastrar(nome, email, senha);
             res.status(200).json(novoUser);
 
         }
-    catch(error){
-            res.status(500).json({error:'Erro ao inserir o novo usuário'});
+        catch (error) {
+            res.status(500).json({ error: 'Erro ao inserir o novo usuário' });
         }
     }
 
-    async localizarUsuarioPeloLogin(req,res){
-            const {login, senha} = req.body;
-        try{
-            const AllUsers = await this.userService.localizarUsuarioPeloLogin(login, senha);
-            res.status(200).json(AllUsers);
+    async localizarUsuarioPeloLogin(req, res) {
+        const { email, senha } = req.body;
+        try {
+            const userLoc = await this.userService.localizarUsuarioPeloLogin(email, senha);
+            res.status(200).json(userLoc);
         }
-        catch{
-            res.status(400).json({error:'Login inválido'});
+        catch {
+            res.status(400).json({ error: 'Login inválido' });
         }
     }
     async localizarUsuarioPeloId(req, res) {
-        const {id} = req.body;
+        const { id } = req.body;
         try {
             const idUser = await this.userService.localizarUsuarioPeloId(id);
             res.status(200).json(idUser);
-        } catch(error) {
+        } catch (error) {
             res.status(500).json({ error: 'Erro ao buscar id' });
+        }
+    }
+
+    async listarTodos(req,res) {
+        const token = req.headers.authorization;
+        try {
+            const users = await this.userService.listarTodos(token);
+            res.status(200).json(users);
+        } catch (error) {
+            res.status(500).json({error: 'Erro ao listar usuários'});
         }
     }
 }
