ad23d583cf0670a86eb0ebce2f769466ca8bcb1d
lucas teixeira <91092558+DRAVINCK@users.noreply.github.com>
2024-05-14 20:33:03 -0300

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,20 @@
 // controllers/ProductController.js
 
 class ProductController {
+  constructor(productService) {
+    this.ProductService = productService;
+  }
+
+  async cadastrarProduto(req, res) {
+    const { nome, ativo } = req.body;
+    try {
+      const product = await this.ProductService.cadastrarProduto( nome, ativo);
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(500).json({ error: "Erro ao inserir o novo produto." });
+    }
+  }
+
   async listarTodos(req, res) {
     try {
       const products = await ProductService.listarTodos(req.headers.authorization);
@@ -10,6 +24,5 @@ class ProductController {
     }
   }
 
-  // Adicione mais métodos conforme necessário
 }
 module.exports = ProductController
