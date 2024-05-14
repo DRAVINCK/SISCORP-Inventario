2cf5f2f2c11d376ec49b2551a238cc28d9a0a943
lucas teixeira <91092558+DRAVINCK@users.noreply.github.com>
2024-05-14 20:34:44 -0300

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -9,8 +9,16 @@ const ProductServiceInstance = new ProductService(db.Product);
 const ProductController = require('../controllers/productController');
 const ProductControllerInstance = new ProductController(ProductServiceInstance);
 
+router.get('/', function(req, res, next) { 
+  res.send('Modulo de usuario está funcionando');
+});
+
 router.get('/listarTodos', function(req, res, next) {
   ProductControllerInstance.listarTodos(req, res);
+});
+
+router.post('/cadastrarProduto', function(req,res,next){
+  ProductControllerInstance.cadastrarProduto(req,res);
 });
 
 module.exports = router;
